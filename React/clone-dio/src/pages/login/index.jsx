eea276591f0b4e6ee3ddcae5f1ad28fb0f4869cd
Juliana Marques <julianamarqp@gmail.com>
2022-12-21 17:16:36 -0300

    ajustes_

--- React/clone-dio/src/pages/login/index.jsx
+++ React/clone-dio/src/pages/login/index.jsx
@@ -1,4 +1,4 @@
-import { mdEmail, mdLock } from 'react-icons/md';
+import { MdEmail, MdLock } from 'react-icons/md';
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -8,6 +8,7 @@ import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 
+import { api } from '../../services/api';
 import { Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from './styles';
 
 const schema = yup.object({
@@ -18,15 +19,24 @@ const schema = yup.object({
 const Login = () => {
     const navigate = useNavigate();
 
-    const { control, handleSubmit, formState: { errors, isValid } } = useForm({
+    const { control, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(schema),
         mode: 'onChange',
     });
-    const onSubmit = data => console.log(data);
 
-    const handleClickSignIn = () => {
-        navigate('/feed')
-    }
+    const onSubmit = async formData => {
+        try{
+            const { data } = await api.get(`users?email${formData.email}&senha=${formData.password}`);
+             if(data.length === 1){
+                navigate('/feed')
+             } else {
+                alert('Email ou senha inválido')
+             }
+        }catch{
+            alert('Houve um erro, tente novamnete.')
+        }
+    };
+
     return (<>
         <Header />
         <Container>
@@ -40,8 +50,8 @@ const Login = () => {
                     <TitleLogin>Faça seu cadastro</TitleLogin>
                     <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
                     <form onSubmit={handleSubmit/onSubmit}>
-                        <Input name="email" errorMessage={errors?.email?.message} control={control} placeholder="E-mail"/>
-                        <Input name="password" errorMessage={errors?.password?.message} control={control} placeholder="Senha" type="password"/>
+                        <Input name="email" errorMessage={errors?.email?.message} control={control} placeholder="E-mail" leftIcon={<MdEmail/>}/>
+                        <Input name="password" errorMessage={errors?.password?.message} control={control} placeholder="Senha" type="password" leftIcon={<MdLock />}/>
                         <Button title="Entrar" variant="secondary" type="submit"/>
                     </form>
                     <Row>
