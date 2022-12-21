ff6cdfd0184312d238e2c0b41592b27e93fac189
Juliana Marques <julianamarqp@gmail.com>
2022-12-21 17:12:20 -0300

    iniciando-desafio01-calculadora

---
+++ React/desafio01-calculadora/src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 30px;
+    border: 1px solid #c0c0c0;
+    background-color: #ffaa00;
+    color: #ffffff;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
